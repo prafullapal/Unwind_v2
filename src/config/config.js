9ba152e741681e235adaac68c4b9df250854e60e
prafullapal <prafullpal18@gmail.com>
2024-03-03 20:50:58 +0530

    Update PostCard and RTE components, and add TinyMCE API key to config

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ const config = {
 	databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 	collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
 	bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+	tinymceAPIKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
 };
 
 export default config;
